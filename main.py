e5a4a08a864edf869cd47fe03d96a68bc460d239
Justin Cueva <justincueva@Justins-MacBook-Pro.local>
2022-06-27 12:03:42 -0700

    added csv file

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-import csv
-
-with open('weather_data.csv') as data_file:
-    data = csv.reader(data_file)
-    print(data)
-    temperatures = []
-    for row in data:
-        if row[1] != 'temp':
-            single_temperature = int(row[1])
-            temperatures.append(single_temperature)
-
-    print(temperatures)
+print('squires')
